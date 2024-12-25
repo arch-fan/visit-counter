3811f4202b3d3263689428070caada7f9b6c2b90
arch-fan <juanserracines@gmail.com>
2024-12-25 12:10:18 +0100

    no-cache header

--- pkg/routes/counter.go
+++ pkg/routes/counter.go
@@ -30,6 +30,7 @@ func GetCount(db *gorm.DB) http.HandlerFunc {
 		}
 
 		w.Header().Set("Content-Type", "image/svg+xml")
+		w.Header().Set("Cache-Control", "no-cache")
 		w.Write([]byte(page.CreateSVG()))
 		go func() {
 			db.Model(&page).Update("visits", page.Visits+1)
